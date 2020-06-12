90f55a4f3de6cef303a119764313a613570880fe
istiak <=>
2020-06-12 20:02:35 +0600

    made a custom command

--- api/routes/console.php
+++ api/routes/console.php
@@ -16,3 +16,7 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+// Artisan::command('kala',function(){
+//     $this->info('black lives matter ') ;
+// })->describe('recent times ');
